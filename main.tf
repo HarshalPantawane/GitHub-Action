2921eab8df8a373631cc443e3bb032587a78c13a
Harshal Pantawane <hmpantawane2214@gmail.com>
2025-11-25 01:40:09 +0530

    github-action

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "dev-vpc"
+  }
+}
 
+resource "aws_subnet" "subnet_1" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-east-1"
+  tags = {
+    Name = "public_subnet"
+  }
+}
