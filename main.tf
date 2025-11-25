8e4b910bf38a779aaf28059d3fdc615e80b309ea
Harshal Pantawane <157274991+HarshalPantawane@users.noreply.github.com>
2025-11-25 15:45:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "subnet_1" {
   cidr_block = "10.0.0.0/24"
   vpc_id = aws_vpc.my_vpc.id
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "public_subnet"
   }
